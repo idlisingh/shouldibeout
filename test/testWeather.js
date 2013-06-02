925366f028134680b3624751f90f26ffbab83bc1
idlisingh <idlisingh@outlook.com>
2013-06-02 09:50:15 -0400

    trying to get test to work

--- test/testWeather.js
+++ test/testWeather.js
@@ -1,3 +1,13 @@
 var weather = require('../weather.js');
+var http = require('http');
 
-weather.interpret('07310', null);
+var response = http.OutgoingMessage.prototype
+response.writeHead = function (num, string) {
+	console.log(string);
+}
+response.write = function(obj) {
+	console.log(obj);
+}
+response.end = function() {}
+
+weather.interpret('07310', new http.OutgoingMessage());
