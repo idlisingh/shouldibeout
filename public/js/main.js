b98865a635d9cbb6a378dcc3b79118347c95db43
idlisingh <idlisingh@outlook.com>
2013-06-03 23:54:37 -0400

    Changed error condition

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,11 @@ function getWeather() {
         function(data) {
           var weatherValue = JSON.parse(data);
           $('#shouldResult').fadeOut(300, function() {
-            $('#shouldResult').html('<font style="color:black;font-size:50pt">Location: ' + weatherValue.city + '<br/><br/> ' + weatherValue.should + '</font>');
+            if (weatherValue.should == "Error occured") {
+              $('#shouldResult').html('<font style="color:black;font-size:50pt">I dont know where \"' + zipCode + '\" is!!</font>');
+            }else {
+              $('#shouldResult').html('<font style="color:black;font-size:50pt">In ' + weatherValue.city + '<br/><br/> ' + weatherValue.should + '</font>');
+            }
             $('#shouldResult').fadeIn(300);
           });
       });
